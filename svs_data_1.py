538b540b8cff5496dd35b69d53ed94c0fa0b4444
YutongZhang <yutongzhang@10-16-110-129.dynapool.wireless.nyu.edu>
2019-04-15 20:28:04 -0400

    first commit

---
+++ svs_data_1.py
@@ -0,0 +1,25 @@
+class LabeledList():
+    def __init__(self, data = None, index = None):
+        self.values = data
+        self.index = index
+
+    def __str__(self):
+        return format("str(self.values)", ">20s") + str(self.index)
+
+    def __getitem__(self,item):
+        empty_list = []
+        print(self.index)
+        for a in item:
+            if a in self.index:
+                for index in self.index:
+                    if index == a:
+                        index_val = item.index(a)
+                        empty_list.append(self.values[index_val])
+        return empty_list
+    
+    def __setitem__(self, key, value):
+        for i in range (0, len(self.values)):
+            if self.index[i] == key:
+                self.values[i] = value
+        return self
+
